272383a3dbccece986050b7a593eca5ead711669
aaditya <aaditwocode@gmail.com>
2024-05-01 01:21:16 +0530

    did stl

---
+++ C++ STL/map.cpp
@@ -0,0 +1,31 @@
+// map<int,string> m;
+// m[1]="aa";
+// m.insert(3,"aa");
+// vector<int>:: iterator it=v.begin();
+// for( it=v.begin();it!=v.end();it++){
+//     cout<<(*it).first<<it->second;
+
+// //for(auto p: m){
+//     m.first;
+//     m.second;
+// }
+
+// // auto it =m.find(3);
+// if(it==m.end())
+// {
+//     no value
+// }
+// else{
+//     it->first it->second;
+// }
+
+
+
+//  m.erase(3);
+//m.erase(it);      if it has something delete it else segment fault;
+
+//m.clear();
+// keys are unique in map
+
+
+unordered_map
---
+++ C++ STL/vector.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// void printvec (vector<int>v){
+//     cout<<v.size();
+//     for(int i=0;i<v.size();i++){
+//         cout<<v[i]<<endl;
+//     }
+// }
+
+int main(){
+// //     vector<int>v;
+// //     int n;
+// //    for(int i=0;i<n;i++){
+// //     int x;
+// //     cin>>x;
+// //     printvec(v);
+
+// //     v.push_back(x);
+// //    }
+
+// vector<int>v(10,7);
+// v.pop_back();
+
+// //  printvec(v);
+
+
+vector<pair<int,int>>v={{1,2}, {2,3}};
+// //    for(int i=0;i<n;i++){
+// //     int x;
+// //     cin>>x;
+// //     cin>>y;
+// // v.push_back({x,y});
+// //}
+// int N;
+// cin>>N;
+// vector<int>v[N];
+// for(int i=0;i<n;i++){
+//     int n;
+//     cin>>n;
+//     for(int j=0;j<n;j++){
+//         int x;
+//         cin>>x;
+//         v[i].push_back(x);
+//     }
+// }
+
+vector<vector<int>>v;
+//just different vector<int>temp;
+// temp.push_back(x)
+// v.push_back(temp);
+
+ return 0;
+}
